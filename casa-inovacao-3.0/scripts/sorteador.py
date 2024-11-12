32dbf60a79665201c4fefb7e3690b9558479fd25
Natan Cabral <102121764+Ntzim@users.noreply.github.com>
2024-11-12 00:21:50 -0300

    Update sorteador.py

--- casa-inovacao-3.0/scripts/sorteador.py
+++ casa-inovacao-3.0/scripts/sorteador.py
@@ -81,7 +81,7 @@ def baixar_excel(df, filename):
 
 # Configuração da aplicação
 st.title("Sorteio Edital | Casa da Inovação")
-st.image(r'C:\Users\natan\Documents\Besouro\ID_CASA_INOVACAO 1.png')
+st.image('casa-inovacao-3.0/imagens/ID_CASA_INOVACAO 1.png')
 
 # Seletores de curso
 curso_selecionado = st.selectbox("Selecione o curso", [
